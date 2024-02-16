80580b709e4e95524aa3bd9db06aee4cdf70072a
Volodymyr Rusyn <volodymyrruso@gmail.com>
2024-02-16 16:14:13 +0100

    fixed book

---
+++ src/main/java/com/example/bookstore/model/Book.java
@@ -0,0 +1,30 @@
+package com.example.bookstore.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.math.BigDecimal;
+import lombok.Data;
+
+@Data
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "title", nullable = false)
+    private String title;
+    @Column(name = "author", nullable = false)
+    private String author;
+    @Column(name = "isbn", unique = true, nullable = false)
+    private String isbn;
+    @Column(name = "price", nullable = false)
+    private BigDecimal price;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "cover_image")
+    private String coverImage;
+}
