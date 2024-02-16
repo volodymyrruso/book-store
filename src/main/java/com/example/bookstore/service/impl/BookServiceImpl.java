6451524ff21d487731e952751a25c7f7a7222581
Volodymyr Rusyn <volodymyrruso@gmail.com>
2024-02-16 15:45:02 +0100

    git work flow

---
+++ src/main/java/com/example/bookstore/service/impl/BookServiceImpl.java
@@ -0,0 +1,25 @@
+package com.example.bookstore.service.impl;
+
+import com.example.bookstore.model.Book;
+import com.example.bookstore.repository.BookRepository;
+import com.example.bookstore.service.BookService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BookServiceImpl implements BookService {
+
+    private final BookRepository repository;
+
+    @Override
+    public Book save(Book book) {
+        return repository.save(book);
+    }
+
+    @Override
+    public List<Book> findAll() {
+        return repository.findAll();
+    }
+}
