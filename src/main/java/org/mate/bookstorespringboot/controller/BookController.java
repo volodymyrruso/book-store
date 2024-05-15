8eaa71d7635ad7ef33e0052edf2960a38d5f2b3b
volodymyrruso <130042707+volodymyrruso@users.noreply.github.com>
2024-05-15 14:30:32 +0200

    Update BookController.java

--- src/main/java/org/mate/bookstorespringboot/controller/BookController.java
+++ src/main/java/org/mate/bookstorespringboot/controller/BookController.java
@@ -8,7 +8,6 @@ import org.mate.bookstorespringboot.service.interfaces.BookService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
